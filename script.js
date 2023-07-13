810b317fa8be67dc50a494c58b6624b3b6751020
Dan Anttila <115313515+D-Antt@users.noreply.github.com>
2023-07-13 15:22:31 -0400

    done with the follow along and I feel much better about it.  Just needs some CSS

--- script.js
+++ script.js
@@ -1,18 +1,68 @@
 //this is cheaper computationally
-const pokeCon = document.querySelector("#pokeCon img");
+const imgContainer = document.querySelector("#pokeCon img");
+const pokeName = document.getElementById("name");
+const pokeSpecies = document.getElementById("species");
+const pokeWeight = document.getElementById("weight");
+const pokeHeight = document.getElementById("height");
+const abilitiesList = document.getElementById("abilitiesList");
+const getPokeButton = document.getElementById("getPokeButton");
+//utility functions
+function setImgSrcAndAlt(imgNode, imgSrc, imgAlt){
+    imgNode.src = imgSrc;
+    imgNode.alt = imgAlt;
+}
 
-//here is where we put the caption
-const caption = document.getElementById("poke-img-caption");
+function setNodeText(domNode,text){
+    domNode.innerText = text;
+}
 
+//project-specific function
+
+function setPokeBasics(pokemon) {
+    setNodeText(pokeName,`NAME: ${pokemon.name}`);
+    setNodeText(pokeSpecies,`SPECIES: ${pokemon.species.name}`);
+    setNodeText(pokeWeight, `HEIGHT: ${pokemon.height}`);
+    setNodeText(pokeHeight, `WEIGHT: ${pokemon.weight}`);
+}
+
+function setPokeAbilities(pokemon) {
+    let htmlString = '';
+    pokemon.abilities.forEach((item)=>{
+        htmlString += `<li>${item.ability.name}</li>`;
+    });
+    abilitiesList.innerHTML = htmlString;
+}
+
+function createPokeProfile(pokemon){
+    const imgSrc = pokemon.sprites.other["official-artwork"].front_default;
+    setImgSrcAndAlt(imgContainer, imgSrc, pokemon.name);
+    setPokeBasics(pokemon);
+    setPokeAbilities(pokemon);
+}
+
+function getRandomPokemon(){
+    const randomPokeID = Math.floor(Math.random() * 1000 +1);
+    console.log(randomPokeId);
 //get the data from API
-fetch("https://pokeapi.co/api/v2/pokemon/56")
+fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokeID}`)
 .then((res) => res.json())
 .then((json) => {
     const pokemon = json;
-    const name = pokemon.name;
-    const imgSrc = pokemon.sprites.other["official-artwork"].front_default;
-    caption.innerText = name;
-    pokeCon.src = imgSrc;
-    pokeCon.alt = name;
+    createPokeProfile(pokemon)
 })
 .catch((err) => console.log(err));
+}
+
+//get the data from API
+fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokeID}`)
+.then((res) => res.json())
+.then((json) => {
+    const pokemon = json;
+    createPokeProfile(pokemon)
+})
+.catch((err) => console.log(err));
+
+getPokeButton.addEventListener("click", function () {
+    getRandomPokemon();
+});
+getRandomPokemon();
